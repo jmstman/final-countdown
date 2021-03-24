9e3d13bc729a10bc1b0c526e67178547629ab59c
jmstman <79401578+jmstman@users.noreply.github.com>
2021-03-24 17:47:31 +0100

    Update script.js (test)
    changed brackets to parenthesis and added pokemon name prior to

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // array of strings
 let pokemonList = [
-  {name: "Charmander", height: 0.6, type: ["blaze","solar-power"]},
-  {name: "Machamp", height: 1.6, type: ["guts","steadfast","no-guard"]},
-  {name: "Lugia", height: 5.2, type: ["pressure","multiscale"]},
+  pokemon (name: "Charmander", height: 0.6, type: ["blaze","solar-power"]),
+  pokemon (name: "Machamp", height: 1.6, type: ["guts","steadfast","no-guard"]),
+  pokemon (name: "Lugia", height: 5.2, type: ["pressure","multiscale"]),
 ];
 console.log(pokemonList);
