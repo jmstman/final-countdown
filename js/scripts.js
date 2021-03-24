1f9c149008311b98f47ce99eeb90c42ec2cb28bc
jmstman <79401578+jmstman@users.noreply.github.com>
2021-03-24 16:22:31 +0100

    Update scripts.js
    Created an array list of Pokemons by name, height and types

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-alert('Hello world');
-let favoriteFood = 'Steak';
-document.write(favoriteFood);
+// array of strings
+let pokemonList = [];
+
+let pokemons = [{
+  name: "Charmander",
+  height: 0.6,
+  types: ["monster", "dragon"]
+};
+
+pokemons = {
+  name: "Machamp",
+  height: 1.6,
+  types: "human-like",
+};
+
+pokemons = {
+  name: "Lugia",
+  height: 5.2,
+  types: "no eggs",
+}];
