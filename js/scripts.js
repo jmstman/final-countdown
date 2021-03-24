e30df1668c83379a0f090056b06d134fc008b14b
jmstman <79401578+jmstman@users.noreply.github.com>
2021-03-24 16:31:59 +0100

    Update scripts.js
    Adjusted format for pokemon list array

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,7 @@
 // array of strings
-let pokemonList = [];
-
-let pokemons = [{
-  name: "Charmander",
-  height: 0.6,
-  types: ["monster", "dragon"]
-};
-
-pokemons = {
-  name: "Machamp",
-  height: 1.6,
-  types: "human-like",
-};
-
-pokemons = {
-  name: "Lugia",
-  height: 5.2,
-  types: "no eggs",
-}];
+let pokemonList = [
+  {name: "Charmander", height: 0.6, type: ["blaze","solar-power"]},
+  {name: "Machamp", height: 1.6, type: ["guts","steadfast","no-guard"]},
+  {name: "Lugia", height: 5.2, type: ["pressure","multiscale"]},
+];
+console.log(pokemonList);
