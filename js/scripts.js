e7259ebc53a6a3a1e6add2175e512472491e622b
jmstman <79401578+jmstman@users.noreply.github.com>
2021-03-25 09:32:48 +0100

    Update scripts.js
    changed pokemon types to correct group classification

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // array of strings
 let pokemonList = [
-  {name: "Charmander", height: 0.6, type: ["blaze","solar-power"]},
-  {name: "Machamp", height: 1.6, type: ["guts","steadfast","no-guard"]},
-  {name: "Lugia", height: 5.2, type: ["pressure","multiscale"]},
+  {name: "Charmander", height: 0.6, type: ["fire"]},
+  {name: "Machamp", height: 1.6, type: ["fighting"]},
+  {name: "Lugia", height: 5.2, type: ["psychic","flying"]},
 ];
 console.log(pokemonList);
