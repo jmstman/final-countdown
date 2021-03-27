7a16e0d456ed82d78cbdce632d416b68d537889a
jmstman <79401578+jmstman@users.noreply.github.com>
2021-03-27 13:19:42 +0100

    Update scripts.js
    Made array more readable for each character

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 // array of strings
 let pokemonList = [
-  {name: "Charmander", height: 0.6, type: ["fire"]},
-  {name: "Machamp", height: 1.6, type: ["fighting"]},
-  {name: "Lugia", height: 5.2, type: ["psychic","flying"]},
+  {
+    name: "Charmander",
+    height: 0.6,
+    type: ["fire"]
+  },
+  {
+    name: "Machamp",
+    height: 1.6,
+    type: ["fighting"]
+  },
+  {
+    name: "Lugia",
+    height: 5.2,
+    type: ["psychic","flying"]
+  }
 ];
 console.log(pokemonList);
