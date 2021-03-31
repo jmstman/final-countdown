58ed68d1819b0197d8e9109990e293355fd32e6b
jmstman <79401578+jmstman@users.noreply.github.com>
2021-03-31 16:25:08 +0200

    Revert "Update scripts.js"
    This reverts commit 3550335c177081632519aea6e4ee9e6b08d06cc5.

--- js/scripts.js
+++ js/scripts.js
@@ -20,20 +20,25 @@ pokemonList = [
   {
     name: "Charmander",
     height: 0.6,
-    type: ["Fire"]
+    type: ["fire"]
   },
   {
     name: "Machamp",
     height: 1.6,
-    type: ["Fighting"]
+    type: ["fighting"]
   },
   {
     name: "Lugia",
     height: 5.2,
-    type: ["Psychic","Flying"]
-  }ke
+    type: ["psychic","flying"]
+  }
 ];
 
-pokemonList.forEach(function(pokemon) {
-   console.log(pokemon.name + " " +pokemon.height + " " + pokemon.type);
- });
+let sizeThreshold = 5.2;
+
+for (let i = 0; i < pokemonList.length; i++){
+  if (pokemonList[i].height >= sizeThreshold){
+    document.write(`${pokemonList[i].name}  (Height: ${pokemonList[i].height}) - Wow!! This is huge! <br>`)
+  } else { document.write(`${pokemonList[i].name}  (Height: ${pokemonList[i].height}) <br>`)}
+
+}
