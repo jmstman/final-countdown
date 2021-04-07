ddf376c34dacd79eee2ac95288b23d4394a2450e
jmstman <79401578+jmstman@users.noreply.github.com>
2021-04-07 14:24:29 +0200

    Added loadDetails, showDetails to scripts.js
    Added loadDetails, showDetails to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,16 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 function add(pokemon) {
     if (
       typeof pokemon === "object" &&
-      "name" in pokemon &&
-      "height" in pokemon &&
-      "types" in pokemon
+      "name" in pokemon
     ) {
       pokemonList.push(pokemon);
     } else {
       console.log("pokemon is not correct");
     }
   }
+
   function getAll() {
-    return repository;
-  }
-  // details of pokemon in console
-  function showDetails(pokemon) {
-    console.log(pokemon);
+    return pokemonList;
   }
   function addListItem(pokemon){
     let pokemonList = document.querySelector(".pokemon-list");
@@ -66,18 +61,25 @@ function loadList() {
       console.error(e);
     });
   }
+
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
+  }
 // function outside of IIFE
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
   };
 })();
-// added pokemon to list
-pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
 
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
